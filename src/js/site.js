e0ede46a94991177f25b50e3f047691bc6d4b1c6
Chris Tsoi <chrisjtsoi.work@gmail.com>
2017-05-28 21:45:41 +0100

    Added nav links, styled buttons and included tooltips for project icons

--- src/js/site.js
+++ src/js/site.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   $('.parallax').parallax();
-});
-
-$(document).ready(function(){
-      $('.slider').slider();
+  $('.slider').slider();
+  $(".button-collapse").sideNav({
+    menuWidth: 200,
+    edge: 'right',
+    closeOnClick: true,
+    draggable: true
+  });
 });
